a4c67342cfda2440d60360ba0dbe410814929b69
Jeffery <jpereira1@mail.csuchico.edu>
2018-01-25 13:29:24 -0800

    Search part is looking NICE

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,6 +1,7 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Output, Component, Input, EventEmitter, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { DataService } from '../data.service';
+import { Order } from '../models/order';
 
 import { Jewel } from '../models/jewel';
 @Component({
@@ -15,12 +16,33 @@ export class SearchComponent
   search: string = "";
   jewelry: Array<Jewel> = [];
   theJewelry: Array<Jewel> = [];
+  order = new Order();
+  cartNumber: number = 0;
+  @Output() cNumber: EventEmitter<number> = new EventEmitter<number>();
+  @Output() jewel: EventEmitter<Jewel> = new EventEmitter<Jewel>();
+  config: SwiperOptions = {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    spaceBetween: 30,
+    slidesPerColumnFill: 'row'
+  };
+
+
   constructor(private route: ActivatedRoute, private dataService: DataService)
   {
     this.dataService.getJewelry().subscribe(res => this.delegateForJewelry(res));
     this.route.queryParams.subscribe(params => {
       this.search = params["search"]; 
     });
+    var aOrder = JSON.parse(localStorage.getItem('currentOrder'));
+    if(aOrder != null)
+    {
+      this.order = aOrder;
+    }
+    this.cartNumber = this.order.jewelry.length;
+
   }
 
   public delegateForJewelry(jewelry: Array<Jewel>)
